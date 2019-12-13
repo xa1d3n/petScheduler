33871dd4e7b0f62bb6bf282695004e25f3087d9e
Russell Stern <skier2k5@gmail.com>
2019-12-13 12:27:02 -0500

    Fixed a react ujs issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -15,3 +13,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+const images = require.context("../../assets/images", true);
+const imagePath = name => images(name, true);
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
